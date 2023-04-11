46940bd59a07ab09ad0bb5d016bdb980a8160e79
Lisandro Pacheco <lisandrojpm@gmail.com>
2023-04-11 11:34:36 -0500

    Se crea la clase circulo

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace Figura
         double a;
         double alto;
         double ancho;
+        double radio;
 
         Console.WriteLine("Ingrese la base del triangulo");
         b =  Double.Parse(Console.ReadLine());
@@ -25,18 +26,24 @@ namespace Figura
         Console.WriteLine("Ingrese el ancho del rectangulo");
         ancho = Double.Parse(Console.ReadLine());
 
+        Console.WriteLine("Ingrese el radio del circulo");
+        radio = Double.Parse(Console.ReadLine());
+
 
 
 
         var figura2 = new Triangulo(a,b);
         var figura3 = new Rectangulo(alto,ancho);
+        var figura4 = new Circulo(radio);
 
         figura1.MetodoArea();
         figura2.MetodoArea();
         figura3.MetodoArea();
+        figura4.MetodoArea();
        
        Console.WriteLine($"El area del triangulo de base  {figura2.base_t} y altura  {figura2.Altura} es : " +figura2.GetArea());  
        Console.WriteLine($"El area del rectangulo de alto {figura3.alto} y altura  {figura3.ancho} es : " +figura3.GetArea());  
+       Console.WriteLine($"El area del circulo de radio {figura4.radio} es : " +figura4.GetArea());  
 
         }
     }
